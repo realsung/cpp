f8aa0ba6afb174fc39ccb785679e1051ef5b12b9
Realsung <32904385+realsungs@users.noreply.github.com>
2018-07-27 18:04:32 +0000

    c++ standard library , namespace , cout , endl
    Programs that receive and output numbers

--- study/Day1.cpp
+++ study/Day1.cpp
@@ -10,7 +10,7 @@ int main()
 
 /*
 #include <iostream> c++에서 표준 입출력에 필요한 것들을 포함하고 있다.
-c++은 헤더 파일 이름 뒤에 .h가 붙지 않음
+c++은 헤더 파일 이름 뒤에 .h가 붙지 않는다.
 using namespace std는 std라는 이름공간을 우선적으로 참조한다라는 뜻이고
 처음에 using namespace std를 명시해주면 일일이 std::을 적을 필요가 없다.
 cout(consol output)는 표준 출력 함수
